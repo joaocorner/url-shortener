62a6865b36ce5c87ab7263790d43a8872f7920db
joaocorner <joaoeduardocorner@gmail.com>
2022-11-01 20:37:27 -0300

    adjusting home screen style

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,18 +3,17 @@ import "./home.scss";
 
 const Home = () => {
   return (
-    <div>
-      <div>
-        <form action="">
-          <div className="container-input">
-            <input type="text" />
-            <button type="submit">Encurtar URL</button>
-          </div>
-        </form>
-      </div>
+    <div className="container">
+      <h1>URL Shortener</h1>
+      <form action="">
+        <div className="container-input">
+          <input type="text" />
+          <button type="submit">Encurtar URL</button>
+        </div>
+      </form>
       <div className="container-output">
-        <p className="long-link"></p>
-        <p className="shortener-link"></p>
+        <p className="long-link">https://www.youtube.com/</p>
+        <p className="shortener-link">https://www.yt.com/</p>
       </div>
     </div>
   );

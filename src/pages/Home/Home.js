a102aa7db333e6385130133bce32aa27b85d83eb
joaocorner <joaoeduardocorner@gmail.com>
2022-11-02 23:32:35 -0300

    creating navbar

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,19 +1,22 @@
-import React from "react";
 import "./home.scss";
+import Nav from "../../components/Nav";
 
 const Home = () => {
   return (
-    <div className="container">
-      <h1>URL Shortener</h1>
-      <form action="">
-        <div className="container-input">
-          <input type="text" />
-          <button type="submit">Encurtar URL</button>
+    <div>
+      <Nav />
+      <div className="container">
+        <h1>URL Shortener</h1>
+        <form action="">
+          <div className="container-input">
+            <input type="text" />
+            <button type="submit">Encurtar URL</button>
+          </div>
+        </form>
+        <div className="container-output">
+          <p className="long-link">https://www.youtube.com/</p>
+          <p className="shortener-link">https://www.yt.com/</p>
         </div>
-      </form>
-      <div className="container-output">
-        <p className="long-link">https://www.youtube.com/</p>
-        <p className="shortener-link">https://www.yt.com/</p>
       </div>
     </div>
   );
